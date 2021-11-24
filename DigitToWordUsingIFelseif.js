0bfde1297e33dc2ffc305006db0518595768244c
priyanka897-star <priyankamore8997@gmail.com>
2021-11-25 01:04:14 +0530

    Added:Functionlity to convert digit two word using if else

---
+++ DigitToWordUsingIFelseif.js
@@ -0,0 +1,45 @@
+let digit = 2;
+let one=1;
+let two=2;
+let three =3;
+let four =4;
+let five =5;
+let six = 6;
+let seven =7;
+let eight=8 
+let nine =9
+let ten =10
+
+    if(digit==one)
+        console.log("One");
+    
+    else if(digit==two)
+        console.log("Two");
+    
+    else if(digit==three)
+        console.log("Three");
+    
+     else if(digit==four)
+        console.log("Four");
+     
+    else if(digit==five)
+        console.log("Five");
+    
+    else if(digit==six)
+        console.log("Six");
+    
+    else if(digit==seven)
+        console.log("Seven");
+    
+    else if(digit==8)
+        console.log("Eight");
+    
+    else if(digit==9)
+        console.log("Nine");
+    
+     else if(digit == 0)
+        console.log("Zero");
+     
+      
+
+    
