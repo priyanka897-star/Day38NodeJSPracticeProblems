6d39f99f2cff979d65376e5f07c166ab0115639c
priyanka897-star <priyankamore8997@gmail.com>
2021-11-25 01:28:31 +0530

    Added:Functonality to print unit using case statement

---
+++ ReadNumberUnitIfElseIf.js
@@ -0,0 +1,15 @@
+let digit = 1000;
+let one =1;
+let ten=10;
+let hundred =100;
+let thousand=1000;
+ 
+if(digit==one)
+    console.log("one"); 
+ else if(digit ==ten)
+    console.log("Ten");
+else if(digit==hundred)
+    console.log("Hundred");
+else if(digit ==thousand)
+      console.log("Thousand");
+ 
