2a8137ffc82ee54f90e4bbd6ee29cd5a0726e49d
priyanka897-star <priyankamore8997@gmail.com>
2021-11-25 01:18:58 +0530

    Added:Functonality to print week day using if else

---
+++ DisplayWeekDayIfElseif.js
@@ -0,0 +1,24 @@
+ let digit = 2;
+ let sunday =1;
+ let monday=2;
+ let tuesday =3;
+ let wenesday=4;
+ let thursday=5;
+ let friday=6;
+ let saturday=7;
+if(digit==sunday)
+    console.log("Sunday"); 
+ else if(digit ==monday)
+    console.log("Monday");
+else if(digit==tuesday)
+    console.log("Tuesday");
+else if(digit ==wenesday)
+      console.log("Wednesday");
+ else if(digit =thurday)
+    console.log("Thrusday");
+ else if(digit ==friday)
+        console.log("Friday");  
+ else if(digit ==saturday)
+        console.log("Saturday");
+       
+   
