cd70b8abb05ee99e8b4a8eecec5b12bbdbeefdfe
priyanka897-star <priyankamore8997@gmail.com>
2021-11-24 23:48:33 +0530

    Added:Functionlity to convert number to unit using case statement

---
+++ ReadNumberToUnit.js
@@ -0,0 +1,20 @@
+let digit = 100
+let one =1
+let ten =10
+let hundres =100
+let thousand = 1000
+switch(digit){
+    case one:
+        
+        console.log("one");
+        break;
+    case ten:
+        console.log("Ten");
+        break;
+    case hundres:
+        console.log("Hundred");
+        break;
+    case thousand:
+        console.log("Thousand");
+        break;
+}
