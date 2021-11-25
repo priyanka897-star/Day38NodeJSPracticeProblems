6729f1026a4eb6ab77047db626cf1303404ff9ef
priyanka897-star <priyankamore8997@gmail.com>
2021-11-25 23:23:04 +0530

    Added:Functionlity to check two number are palindrome and primeor not

--- checkPrimePalindrome.js
+++ checkPrimePalindrome.js
@@ -6,10 +6,10 @@ const rl = readline.createInterface({
 });
 
 
-function getPalindrome(num1){
+function getPalindrome(num1) {
     let temp = 0, remainder;
     let a = num1;
-    while(a > 0){
+    while (a > 0) {
         remainder = a % 10;
         a = parseInt(a / 10);
         temp = temp * 10 + remainder;
@@ -17,26 +17,26 @@ function getPalindrome(num1){
     return temp;
 }
 
-function checkPrime(number){
+function checkPrime(number) {
     let flag = 0;
-    if(number == 1){
+    if (number == 1) {
         console.log(number + " is neither prime not composite");
-    }else{
-        for(let i = 2; i < number; i++){
-            if(parseInt(number) % parseInt(i) == 0){
+    } else {
+        for (let i = 2; i < number; i++) {
+            if (parseInt(number) % parseInt(i) == 0) {
                 flag = 1;
             }
         }
-        if(flag == 1){
+        if (flag == 1) {
             console.log(number + " is not a prime");
-        }else{
+        } else {
             console.log(number + " is a prime");
         }
     }
 }
 rl.question("enter the  number ", function (number1) {
-let number = parseInt(number1);
-checkPrime(number);
-let reverseNumber = getPalindrome(number);
-checkPrime(reverseNumber);
+    let number = parseInt(number1);
+    checkPrime(number);
+    let reverseNumber = getPalindrome(number);
+    checkPrime(reverseNumber);
 });
